40074f79edb9624d18c24c7a8297876535a178c4
Lightninggabe1 <lightninggabe191@gmail.com>
2024-07-01 10:18:38 +0000

    wwwwww

--- main.py
+++ main.py
@@ -9,3 +9,15 @@ top_genres = ['rap', 'rock', 'pop']
 
 # Write your code below!
 frozen_top_genres = frozenset(top_genres)
+song_data = {'Retro Words': ['pop', 'warm', 'happy', 'electric']}
+
+user_tag_1 = 'warm'
+user_tag_2 = 'exciting'
+user_tag_3 = 'electric'
+
+# Write your code below!
+tag_set = set(song_data['Retro Words'])
+thing = [user_tag_1,user_tag_2,user_tag_3]
+tag_set.update(thing)
+
+song_data['Retro Words'] = tag_set
