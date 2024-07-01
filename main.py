f71342ecdea3bd36c422618807cfa6c5218d9160
Lightninggabe1 <lightninggabe191@gmail.com>
2024-07-01 10:11:02 +0000

    whooooooo my first codespace

---
+++ main.py
@@ -0,0 +1,7 @@
+genre_results = ['rap', 'classical', 'rock', 'rock', 'country', 'rap', 'rock', 'latin', 'country', 'k-pop', 'pop', 'rap', 'rock', 'k-pop',  'rap', 'k-pop', 'rock', 'rap', 'latin', 'pop', 'pop', 'classical', 'pop', 'country', 'rock', 'classical', 'country', 'pop', 'rap', 'latin']
+
+survey_genres = set(['rap', 'classical', 'rock', 'rock', 'country', 'rap', 'rock', 'latin', 'country', 'k-pop', 'pop', 'rap', 'rock', 'k-pop',  'rap', 'k-pop', 'rock', 'rap', 'latin', 'pop', 'pop', 'classical', 'pop', 'country', 'rock', 'classical', 'country', 'pop', 'rap', 'latin'])
+print(survey_genres)
+
+survey_abbreviated = {genre[0:3] for genre in genre_results}
+print(survey_abbreviated)
