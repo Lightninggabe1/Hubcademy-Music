b83d07c342785052caff67fac6a520ae530a9d6a
Lightninggabe1 <lightninggabe191@gmail.com>
2024-07-06 09:53:26 +0800

    Update main.py
    New module: 'Set Intersection'

--- main.py
+++ main.py
@@ -71,3 +71,33 @@ for key, val in song_data.items():
   song_tag_set = set(val)
   user_tag_set = set(user_tag_data[key])
   new_song_data[key] = song_tag_set | user_tag_set
+
+
+#new module: 'set intersection'
+
+song_data = {'Retro Words': ['pop', 'warm', 'happy', 'electronic', 'synth'],
+             'Wait For Limit': ['rap', 'upbeat', 'romance'],
+             'Stomping Cue': ['country', 'fiddle', 'party'],
+             'Lowkey Space': ['electronic', 'dance', 'synth', 'upbeat'],
+             'Back To Art': ['pop', 'sad', 'emotional', 'relationship'],
+             'Blinding Era': ['rap', 'intense', 'moving', 'fast'],
+             'Down To Green Hills': ['country', 'relaxing', 'vocal', 'emotional'],
+             'Double Lights': ['electronic', 'chill', 'relaxing', 'piano', 'synth']}
+
+user_recent_songs = {'Retro Words': ['pop', 'warm', 'happy', 'electronic', 'synth'],
+                     'Lowkey Space': ['electronic', 'dance', 'synth', 'upbeat']}
+
+# Write your code below!
+retrowordsset = set(user_recent_songs['Retro Words'])
+lowkeyspaceset = set(user_recent_songs['Lowkey Space'])
+tags_int = lowkeyspaceset & retrowordsset
+
+recommended_songs = {}
+
+for key,val in song_data.items():
+  for tag in val:
+    if tag in tags_int:
+      if key not in user_recent_songs:
+        recommended_songs[key] = val
+  
+ print(recommended_songs)     
