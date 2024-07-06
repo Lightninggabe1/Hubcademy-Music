dbead15ebe8b9f6b99feff5d5b93039d24416855
Lightninggabe1 <lightninggabe191@gmail.com>
2024-07-06 09:56:21 +0800

    Update main.py: "Set Difference"
    Changed code to 'Soft wrap'. 
    Added new module, "Set Difference",  to add a more accurate recommendation engine.

--- main.py
+++ main.py
@@ -101,3 +101,36 @@ for key,val in song_data.items():
         recommended_songs[key] = val
   
  print(recommended_songs)     
+
+
+
+
+
+
+#new_module: "Set Difference"
+
+song_data = {'Retro Words': ['pop', 'warm', 'happy', 'electronic', 'synth'],
+             'Wait For Limit': ['rap', 'upbeat', 'romance', 'relationship'],
+             'Stomping Cue': ['country', 'fiddle', 'party'],
+             'Lowkey Space': ['electronic', 'dance', 'synth', 'upbeat'],
+             'Back To Art': ['pop', 'sad', 'emotional', 'relationship'],
+             'Blinding Era': ['rap', 'intense', 'moving', 'fast'],
+             'Down To Green Hills': ['country', 'relaxing', 'vocal', 'emotional'],
+             'Double Lights': ['electronic', 'chill', 'relaxing', 'piano', 'synth']}
+
+user_liked_song = {'Back To Art': ['pop', 'sad', 'emotional', 'relationship']}
+user_disliked_song = {'Retro Words': ['pop', 'warm', 'happy', 'electronic', 'synth']}
+
+# Write your code below!
+user_liked_songset = set(user_liked_song['Back To Art'])
+user_disliked_songset = set(user_disliked_song['Retro Words'])
+tag_diff = user_liked_songset.difference(user_disliked_songset)
+
+recommended_songs = {}
+for key,val in song_data.items():
+  for tag in val:
+    if tag in tag_diff:
+      if key not in user_liked_song and key not in user_disliked_song:
+        recommended_songs[key] = val
+
+print(recommended_songs)
