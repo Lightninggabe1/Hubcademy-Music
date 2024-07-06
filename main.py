81c7fb8d4609313178c5320c6972d11a0e2135ce
Lightninggabe1 <lightninggabe191@gmail.com>
2024-07-06 13:44:38 +0800

    Update main.py; new: "Symmetric Difference"
    symmetrical
    /sɪˈmɛtrɪk(ə)l/
    adjective
    
    
    
    made up of exactly similar parts facing each other or around an axis; showing symmetry.
    "the shape of a hill, smooth and symmetrical"

--- main.py
+++ main.py
@@ -134,3 +134,25 @@ for key,val in song_data.items():
         recommended_songs[key] = val
 
 print(recommended_songs)
+
+
+
+#new_module: "Symmetric Difference"
+user_song_history = {'Retro Words': ['pop', 'warm', 'happy', 'electronic', 'synth'],
+                     'Stomping Cue': ['country', 'fiddle', 'party'],
+                     'Back To Art': ['pop', 'sad', 'emotional', 'relationship'],
+                     'Double Lights': ['electronic', 'chill', 'relaxing', 'piano', 'synth']}
+
+friend_song_history = {'Lowkey Space': ['electronic', 'dance', 'synth', 'upbeat'],
+                     'Blinding Era': ['rap', 'intense', 'moving', 'fast'],
+                     'Wait For Limit': ['rap', 'upbeat', 'romance', 'relationship'],
+                     'Double Lights': ['electronic', 'chill', 'relaxing', 'piano', 'synth']}
+
+# Write your code below!
+user_tags = set()
+for key, val in user_song_history.items():
+    user_tags.update(set(val))
+friend_tags = set()
+for key, val in friend_song_history.items():
+    friend_tags.update(set(val))
+unique_tags = set(user_tags ^ friend_tags)
